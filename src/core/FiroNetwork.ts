37f1316ca0a15fb3a12a12bc3c18f0d67a1fce5d
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-11-22 22:52:18 -0500

    Update FiroNetwork.ts

--- src/core/FiroNetwork.ts
+++ src/core/FiroNetwork.ts
@@ -12,7 +12,7 @@ export type Network = {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const FIRO_TEST_NET = {
-  messagePrefix: '\x19Firo Signed Message:\n',
+  messagePrefix: '\x19Homeless Coin Signed Message:\n',
   bech32: 'firo',
   bip32: {
     public: 0x043587cf,
@@ -24,15 +24,15 @@ const FIRO_TEST_NET = {
 };
 
 const FIRO_MAIN_NET = {
-  messagePrefix: '\x19Firo Signed Message:\n',
+  messagePrefix: '\x19Homeless Coin Signed Message:\n',
   bech32: 'firo',
   bip32: {
     public: 0x0488b21e,
     private: 0x0488ade4,
   },
-  pubKeyHash: 0x52,
-  scriptHash: 0x7,
-  wif: 0xd2,
+  pubKeyHash: 0x64,
+  scriptHash: 0x28,
+  wif: 0xe4,
 };
 
 export const network = FIRO_MAIN_NET;
